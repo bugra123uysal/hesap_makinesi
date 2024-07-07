5c4719c10f101fdd5be1d9ad00a37f2d7b82c95d
buğra uysal <uysalbugra134@gmail.com>
2024-07-08 01:11:08 +0300

    git hıb a eklendi ve hesap makinesi kodlanmaya başlandı

---
+++ hesapmkn.py
@@ -0,0 +1,17 @@
+
+
+
+sayıa=float(input("1. sayını giriniz"))
+sayıb=float(input("2. sayıyı giriniz"))
+
+""" formüller """
+""" one topla """
+tpl=float(sayıa+sayıb)
+çkr=float(sayıa-sayıb)
+blm=float(sayıa/sayıb)
+çpr=float(sayıa*sayıb)
+
+print(tpl)
+print(çkr)
+print(blm)
+print(çpr)
