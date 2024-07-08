ad049c66ce2e63fa7c679e1c4753b4459ff36853
buğra uysal <uysalbugra134@gmail.com>
2024-07-08 03:22:48 +0300

    işlem seçme ve hesap makinesinden çıkma eklendi ve hesap makinesi projesi bitti

--- hesapmkn.py
+++ hesapmkn.py
@@ -1,17 +1,76 @@
 
+import keyboard
+print("1 toplam  işlem")
+print(" 2 çıkarma işlem")
+print("3 bölme  işlem")
+print("4 çarpma işlem")
 
 
-sayıa=float(input("1. sayını giriniz"))
-sayıb=float(input("2. sayıyı giriniz"))
+
+
+
+while True:
+   
+     işlm= input("1 toplam , 2 çıkarma , 3 bölme , 4 çarpma çıkmak için 'q' kullanınız")
+     if işlm==('q'):
+         break
+       
+     sayıa=float(input("1. sayını giriniz"))
+     sayıb=float(input("2. sayıyı giriniz"))
+
+     if işlm=='1':
+        tpl=float(sayıa+sayıb)
+        print(tpl)
+
+     elif işlm=='2':
+         çkr=float(sayıa-sayıb)
+         print(çkr)   
+     elif işlm=='3':
+         blm=float(sayıa/sayıb)
+         print(blm)  
+     elif işlm=='4':
+         çpr=float(sayıa*sayıb)
+
+         print(çpr)
+     
+
+   
+     
+
+     
+       
+    
+     
+    
+
+    
+        
+    
+
+
+
+
+
+
+
+          
+
+          
+    
+  
+ 
+    
+    
+
+
+
+
 
 """ formüller """
 """ one topla """
-tpl=float(sayıa+sayıb)
-çkr=float(sayıa-sayıb)
-blm=float(sayıa/sayıb)
-çpr=float(sayıa*sayıb)
 
-print(tpl)
-print(çkr)
-print(blm)
-print(çpr)
+
+
+
+
+
